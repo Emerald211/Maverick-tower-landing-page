0ff4b424670a593c02e8bef08b129f3c804a7910
Emerald211 <aderibigbeemmanuel2004@gmail.com>
2023-04-21 11:29:45 +0100

    done

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -1,15 +1,28 @@
 import "./header.styles.css";
+import messageLogo from "../../assets/Frame.png";
 
 const Header = () => {
   return (
     <div className="header">
-      <h1  data-aos="fade-right"
-     data-aos-offset="300"
-     data-aos-easing="ease-in-sine">
+      <h1
+        data-aos="fade-right"
+        data-aos-offset="300"
+        data-aos-easing="ease-in-sine"
+      >
         Skyrocket Your Hotel Bookings with Our Proven Marketing SYSTEMS - Get
         50+ Direct Bookings in 90 Days!
       </h1>
-      <button  data-aos="zoom-in" className="header-button">Unlock my booking boost now</button>
+      <button data-aos="zoom-in" className="header-button">
+        Unlock my booking boost now
+      </button>
+      <img
+        data-aos="fade-right"
+        data-aos-offset="300"
+        data-aos-easing="ease-in-sine"
+        className="message-logo"
+        src={messageLogo}
+        alt=""
+      />
     </div>
   );
 };
